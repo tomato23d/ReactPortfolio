0c20c0118366487356bb52e820e20cf60e1d03be
tomato23d <anna.a.art@gmail.com>
2023-07-06 14:27:34 +1000

    additional styling

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,16 +3,17 @@ import React from 'react';
 export default function Portfolio() {
   return (
     <div className = 'main'>
-      <h1 className = 'page-title'>Anna's  Portfolio :</h1>
-     
-      <ul>
-    <li> application 1 </li>
-    <li> application 1 </li>
-    <li> application 1 </li>
-    <li> application 1 </li>
-    <li> application 1 </li>
-    <li> application 1 </li>
-    </ul>
+           <h1 className = 'page-title'>Anna's  Portfolio :</h1>
+                <div className = 'page-main'>
+                    <ul>
+                        <li> application 1 </li>
+                        <li> application 1 </li>
+                        <li> application 1 </li>
+                        <li> application 1 </li>
+                        <li> application 1 </li>
+                        <li> application 1 </li>
+                    </ul>
+                </div>
     </div>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,10 +3,12 @@ import React from 'react';
 export default function Resume() {
   return (
     <div className = 'main'>
-      <h1 className = 'page-title'>Resume</h1>
-      <p>
-       The brief summary of Anna's skills can be found on linkedin.
-      </p>
+        <h1 className = 'page-title'>Resume</h1>
+            <div className = 'page-main'>
+                  <p>
+                  The brief summary of Anna's skills can be found on linkedin.
+                  </p>
+            </div>
     </div>
   );
 }
