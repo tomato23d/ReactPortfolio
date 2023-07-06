77167773d2f959b9b8c6c8b24ddd7fa9a3afe6f1
tomato23d <anna.a.art@gmail.com>
2023-07-06 15:56:13 +1000

    additional styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,11 @@ import '../css/style.css';
 export default function Footer() {
   return (
     <div className = 'footer'>
-      <p> Find me on  <a href = "https://www.linkedin.com/in/anna-artamonova-19138826">linkedin</a></p>
-      <p> My profile on   <a href = "https://github.com/tomato23d/">github</a></p>
-
+        <ul className = 'footer-line'>
+      <li> <p>Find me on  <a href = "https://www.linkedin.com/in/anna-artamonova-19138826">linkedin</a></p></li>
+    
+      <li> <p> My profile on  <a href = "https://github.com/tomato23d/">github</a></p> </li>
+      </ul>
     </div>
   );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,13 +6,41 @@ export default function Portfolio() {
            <h1 className = 'page-title'>Anna's  Portfolio :</h1>
                 <div className = 'page-main'>
                     <ul>
-                        <li> application 1 </li>
-                        <li> application 1 </li>
-                        <li> application 1 </li>
-                        <li> application 1 </li>
-                        <li> application 1 </li>
-                        <li> application 1 </li>
-                    </ul>
+                        <li className="portfolio-line"> 
+                            <p> <a href = "https://tomato23d.github.io/Weather-Dashboard">Weather Dashboard</a> </p>
+                            <span className="reference"> <a href = "https://github.com/tomato23d/Weather-Dashboard">GitHub</a> </span>
+                        </li>
+                        
+                        <li className="portfolio-line"> 
+                            <p><a href = "https://tomato23d.github.io/PasswordGenerator">Password Generator</a> </p>
+                            <span className="reference"> <a href = "https://github.com/tomato23d/PasswordGenerator">GitHub</a> </span>
+                        </li>
+                         
+                        <li className="portfolio-line"> 
+                            <p><a href = "https://tomato23d.github.io/TravelandDestinationReviewApp">
+                                Travel Destination Review</a> </p>
+                            <span className="reference"> <a href = "https://github.com/tomato23d/TravelandDestinationReviewApp">GitHub</a> </span>  
+                        </li>
+                        
+                        <li className="portfolio-line"> 
+                            <p> <a href = "https://tomato23d.github.io/22.PostBlog">
+                                Social Network</a></p> 
+                            <span className="reference"> <a href = "https://github.com/tomato23d/22.PostBlog">GitHub</a> </span>    
+                        </li>
+                    
+                        <li className="portfolio-line"> 
+                            <p><a href = "https://tomato23d.github.io/Calendar">
+                                Work Day Scheduler</a> </p>
+                            <span className="reference"> <a href = "https://github.com/tomato23d/Calendar">GitHub</a> </span> 
+                        </li>
+
+                        <li className="portfolio-line"> 
+                            <p> <a href = "https://tomato23d.github.io/Mini-Project.w2">
+                                Web page design reflections</a></p> 
+                            <span className="reference"> <a href = "https://github.com/tomato23d/Mini-Project.w2">GitHub</a> </span> 
+                        </li>
+                    </ul>    
+                
                 </div>
     </div>
   );
